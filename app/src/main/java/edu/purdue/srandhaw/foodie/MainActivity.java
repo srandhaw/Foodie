5ee2f2c183202922af1428228786c5d0c6852824
kartikmittal33 <31484338+kartikmittal33@users.noreply.github.com>
2018-02-24 01:30:54 -0600

    Added fotu option

--- app/src/main/java/edu/purdue/srandhaw/foodie/MainActivity.java
+++ app/src/main/java/edu/purdue/srandhaw/foodie/MainActivity.java
@@ -1,30 +1,41 @@
 package edu.purdue.srandhaw.foodie;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
  EditText searchBar;
 
     Button searchButton;
+    ImageButton cameraButton;
+    ImageView iv;
 
     ListView listView;
     int searchCounter=0;
  ArrayList<String> foodItems = new ArrayList<String>();
     CustomAdapter customAdapter=null;
+    static final int CAM = 1111;
 
 
     @Override
@@ -33,12 +44,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         searchBar = (EditText)findViewById(R.id.etsearchbar);
         searchButton = (Button)findViewById(R.id.buttonsearch);
+        cameraButton = (ImageButton)findViewById(R.id.camera);
         listView = (ListView)findViewById(R.id.listfood);
         searchButton.setOnClickListener(this);
 
 
         customAdapter = new CustomAdapter(MainActivity.this,R.layout.custom_foodlistrow,foodItems);
         listView.setAdapter(customAdapter);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAM);
+            }
+        });
     }
 
     @Override
@@ -53,16 +73,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                  foodItems.add(searchBar.getText().toString());
              }
             }
+            break;
 
             case R.id.btgo:
             {
 
             }
+            break;
 
 
 
         }
     }
+
+
+
+    private File getFile() {
+     File folder = new File("camera_app");
+
+     File image_file = new File(folder,"image.jpg");
+     return image_file;
+
+    }
+
 
 
     class CustomAdapter extends ArrayAdapter<String> {
@@ -98,5 +131,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAM) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            // now we can access the photo using bitmap -> photo
+        }
     }
 }
